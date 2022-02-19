639f575b40cfdd1fba189a220fe3cbb908b01f12
Olga <aref.olga@mail.ru>
2022-02-19 18:41:55 +0400

    My first commit

--- lesson1/src/main/java/ora/Application.java
+++ lesson1/src/main/java/ora/Application.java
@@ -3,7 +3,7 @@ package ora;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" Hello world!");
     }
 
 }
